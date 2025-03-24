af3d7255027d4cb349f21accb45cda48d87d57f8
ZiaZahak <zia.zahak@bbc.co.uk>
2025-03-24 16:20:05 +0000

    Update basicTypes.ts

--- practice-repo/src/playground/basicTypes.ts
+++ practice-repo/src/playground/basicTypes.ts
@@ -2,10 +2,12 @@ interface AudioConfig {
   defaultGain: number;
   enabled: boolean;
   micNames: string[];
+  description: string
 }
 
 const audioSettings: AudioConfig = {
-  defaultGain: "high",
+  defaultGain: 100,
   enabled: true,
   micNames: ["mic1", "mic2"],
+  description: "studio A default config"
 };
