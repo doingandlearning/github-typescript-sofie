220d4cca511a309c1baf51edf5abbcaa6d7633e4
AirbusA330neo <nick.buckley@bbc.co.uk>
2025-03-24 16:31:51 +0000

    changes
    1

--- practice-repo/src/playground/basicTypes.ts
+++ practice-repo/src/playground/basicTypes.ts
@@ -2,10 +2,12 @@ interface AudioConfig {
   defaultGain: number;
   enabled: boolean;
   micNames: string[];
+  channel: number
 }
 
 const audioSettings: AudioConfig = {
-  defaultGain: "high",
+  defaultGain: 10,
   enabled: true,
   micNames: ["mic1", "mic2"],
+  channel: 2
 };
