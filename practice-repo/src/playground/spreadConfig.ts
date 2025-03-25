831cc27feec25da93278af1bf34697b2ca0c2c04
ZiaZahak <zia.zahak@bbc.co.uk>
2025-03-25 15:17:12 +0000

    Update spreadConfig.ts

--- practice-repo/src/playground/spreadConfig.ts
+++ practice-repo/src/playground/spreadConfig.ts
@@ -12,9 +12,25 @@ const baseAudio: AudioConfig = {
 const studioAudio = {
   // TODO: Extend baseAudio, override defaultGain to 15, and add description
 };
+const studioAudio = {
+  ...baseAudio,
+  defaultGain: 15,
+  description: 'Studio B audio config'
+
+};
+  
+
+const studioAudio: AudioConfig = {
+    ...baseAudio,
+    defaultGain: 20,
+    description: 'Studio B audio config'
+
+  };
+
 
 function printConfig(config: AudioConfig) {
   console.log(config);
 }
 
 printConfig(studioAudio);
+
