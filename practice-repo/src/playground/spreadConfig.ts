8c683ba9b5518d1d74c9d7805a2f9170c3402cf7
Marcus McNulty <marcus.mcnulty@bbc.co.uk>
2025-03-25 15:05:48 +0000

    Marcus learning about Spread

--- practice-repo/src/playground/spreadConfig.ts
+++ practice-repo/src/playground/spreadConfig.ts
@@ -9,9 +9,13 @@ const baseAudio: AudioConfig = {
   defaultGain: 10,
 };
 
-const studioAudio = {
   // TODO: Extend baseAudio, override defaultGain to 15, and add description
-};
+
+const studioAudio = {
+  ...baseAudio,
+  defaultGain: 15,
+  description: 'Studio B audio config'
+}
 
 function printConfig(config: AudioConfig) {
   console.log(config);
