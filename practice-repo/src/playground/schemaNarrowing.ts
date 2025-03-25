6a356e127cdd4f29cff14cfb1a12f45ea0bf4895
andyautomation <andrew.dakin@bbc.co.uk>
2025-03-25 11:04:49 +0000

    Update schemaNarrowing.ts

--- practice-repo/src/playground/schemaNarrowing.ts
+++ practice-repo/src/playground/schemaNarrowing.ts
@@ -1,14 +1,18 @@
+//the interface sets the rules of any defined const set against it - in this case 'config'
+
 interface StudioConfig {
   defaultGain?: number;
   micNames?: string[];
 }
 
+//the contents of config is defined by the interface 'StudioConfig'
 const config: StudioConfig = {
   defaultGain: 10,
   micNames: ["mic1", "mic2"],
 };
 
 // TODO: write a function to log each mic name in uppercase
+//the process of this function is to print out all the microphones defined in config
 
 function logMicNames(config: StudioConfig) { //defines a function called logMicNames, takes one parameter called config which is expected to be of type StudioConfig - config contains a property 'micNames'
   if (Array.isArray(config.micNames)) { //check if micNames is and array, if it IS an array, the function proceeds to loop through it
