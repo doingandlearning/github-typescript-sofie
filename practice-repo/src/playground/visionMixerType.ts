8bd4877d27978e4f80b449f83a88b8c3d534d1a7
andyautomation <andrew.dakin@bbc.co.uk>
2025-03-25 13:16:32 +0000

    Update visionMixerType.ts
    config is already defined in another file

--- practice-repo/src/playground/visionMixerType.ts
+++ practice-repo/src/playground/visionMixerType.ts
@@ -9,15 +9,28 @@ interface StudioConfig {
   };
 }
 
-function getMixerName(config: StudioConfig): string {
+function getMixerName(mixerConfig: StudioConfig): string {
   // TODO: Return a human-readable name based on visionMixer type
-  return "";
+    const type = mixerConfig.visionMixer?.type;
+
+    switch (type) {
+
+      case VisionMixerDevice.Atem:
+      return 'Blackmagic Atem Mixer';
+      case VisionMixerDevice.Carbonite:
+      return 'Ross Carbonite Mixer'
+
+      default:
+        return 'unknown mixer'
+
+    }
+
 }
 
-const config: StudioConfig = {
+const mixerConfig: StudioConfig = {
   visionMixer: {
     type: VisionMixerDevice.Atem,
   },
 };
 
-console.log(getMixerName(config));
+console.log(getMixerName(mixerConfig));
